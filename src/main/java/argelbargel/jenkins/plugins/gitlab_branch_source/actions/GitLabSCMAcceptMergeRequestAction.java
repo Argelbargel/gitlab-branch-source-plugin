6c49c5819f7c8ef8e11a196452bf3aa54a44b7c9
Oscar Andreasson <oscar.andreasson@pelagicore.com>
2018-02-26 14:52:55 +0100

    GitLabConnectionProperty.getClient returns GitLabClient
    The GitLabConnectionProperty.getClient method now returns a GitLabClient
    instead of a GitLabApi meaning that several of the usages of the class needs to
    be rewritten slightly.
    
    Signed-off-by: Oscar Andreasson <oscar.andreasson@pelagicore.com>

--- src/main/java/argelbargel/jenkins/plugins/gitlab_branch_source/actions/GitLabSCMAcceptMergeRequestAction.java
+++ src/main/java/argelbargel/jenkins/plugins/gitlab_branch_source/actions/GitLabSCMAcceptMergeRequestAction.java
@@ -2,7 +2,9 @@ package argelbargel.jenkins.plugins.gitlab_branch_source.actions;
 
 
 import com.dabsquared.gitlabjenkins.connection.GitLabConnectionProperty;
-import com.dabsquared.gitlabjenkins.gitlab.api.GitLabApi;
+import com.dabsquared.gitlabjenkins.gitlab.api.GitLabClient;
+import argelbargel.jenkins.plugins.gitlab_branch_source.api.GitLabMergeRequest;
+import com.dabsquared.gitlabjenkins.gitlab.api.model.MergeRequest;
 import hudson.model.InvisibleAction;
 import hudson.model.Run;
 import hudson.model.TaskListener;
@@ -17,27 +19,30 @@ import static java.util.logging.Level.SEVERE;
 public final class GitLabSCMAcceptMergeRequestAction extends InvisibleAction implements Serializable {
     private static final Logger LOGGER = Logger.getLogger(GitLabSCMAcceptMergeRequestAction.class.getName());
 
-    private final int projectId;
-    private final int mergeRequestId;
+    private final int mergeRequestID;
+    private final int mergeRequestPID;
     private final int mergeRequestScopedId;
     private final String commitMessage;
     private final boolean removeSourceBranch;
 
-    public GitLabSCMAcceptMergeRequestAction(int projectId, int mergeRequestId, int mergeRequestScopedId, String commitMessage, boolean removeSourceBranch) {
-        this.projectId = projectId;
-        this.mergeRequestId = mergeRequestId;
+    public GitLabSCMAcceptMergeRequestAction(GitLabMergeRequest MR, int mergeRequestScopedId, String commitMessage, boolean removeSourceBranch) {
+        this.mergeRequestPID = MR.getProjectId();
+        this.mergeRequestID = MR.getId();
         this.mergeRequestScopedId = mergeRequestScopedId;
         this.commitMessage = commitMessage;
         this.removeSourceBranch = removeSourceBranch;
     }
 
     public void acceptMergeRequest(Run<?, ?> build, TaskListener listener) {
-        GitLabApi client = GitLabConnectionProperty.getClient(build);
+        MergeRequest mergeRequest = new MergeRequest();
+        mergeRequest.setProjectId(mergeRequestPID);
+        mergeRequest.setId(mergeRequestID);
+        GitLabClient client = GitLabConnectionProperty.getClient(build);
         if (client == null) {
             listener.getLogger().format("cannot publish build-status pending as no gitlab-connection is configured!");
         } else {
             try {
-                client.acceptMergeRequest(projectId, mergeRequestId, MessageFormat.format(commitMessage, mergeRequestScopedId, build.getFullDisplayName()), removeSourceBranch);
+                client.acceptMergeRequest(mergeRequest, MessageFormat.format(commitMessage, mergeRequestScopedId, build.getFullDisplayName()), removeSourceBranch);
             } catch (Exception e) {
                 listener.getLogger().format("failed to accept merge-request: " + e.getMessage());
                 LOGGER.log(SEVERE, "failed to accept merge-request", e);
